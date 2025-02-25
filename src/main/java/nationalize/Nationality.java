35b56706e64b07d0088e62bcdad6c3fa4407304c
kfrendl <krisztian.frendl@gmail.com>
2025-02-25 12:31:55 +0100

    build: delete the now unused commons-lang3 dependency from pom.xml

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -4,9 +4,7 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
+@lombok.Data
 public class Nationality {
 
     private long count;
@@ -21,35 +19,7 @@ public class Nationality {
         this.countries = countries;
     }
 
-    @Override
-    public String toString() {
-        return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
-    }
-
-    public long getCount() {
-        return count;
-    }
-
-    public void setCount(long count) {
-        this.count = count;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Country> getCountries() {
-        return countries;
-    }
-
-    public void setCountries(List<Country> countries) {
-        this.countries = countries;
-    }
-
+    @lombok.Data
     public static class Country {
 
         private String countryId;
@@ -59,28 +29,5 @@ public class Nationality {
             this.countryId = countryId;
             this.probability = probability;
         }
-
-        @Override
-        public String toString() {
-            return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
-        }
-
-        public String getCountryId() {
-            return countryId;
-        }
-
-        public void setCountryId(String countryId) {
-            this.countryId = countryId;
-        }
-
-        public float getProbability() {
-            return probability;
-        }
-
-        public void setProbability(float probability) {
-            this.probability = probability;
-        }
-
     }
-
 }
